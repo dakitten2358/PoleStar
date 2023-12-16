1084be5bdf3207e75ef24744a175d57e825d96cb
Jay <jason-william.murray@gameloft.com>
2023-12-16 12:43:17 -0500

    more options for input actions

--- Source/PoleStar/Private/TestNodeAction_InputAction.cpp
+++ Source/PoleStar/Private/TestNodeAction_InputAction.cpp
@@ -5,21 +5,80 @@
 #include "GameFramework/PlayerController.h"
 #include "Engine/LocalPlayer.h"
 
-ETestNodeActionResult UTestNodeAction_InputAction::OnTestNodeTick(const FVector& Node, TObjectPtr<APawn> Pawn, float DeltaTime)
+ETestNodeActionResult UTestNodeAction_InputAction::OnTestNodeStart(const FVector& NodeLocation, TObjectPtr<APawn> Pawn)
 {
+	// reset back to defaults
+	CurrentState = EInputActionState::PreAction;
+	CurrentPreActionDuration = 0.0f;
+	CurrentHoldDuration = 0.0f;
+	CurrentPostActionDuration = 0.0f;
+
+	// make sure action is OK
+	if (!IsValid(InputAction))
+		return ETestNodeActionResult::Failed;
+
+	// get a ref to input system
 	if (APlayerController* PC = Pawn->GetController<APlayerController>())
 	{
 		if (ULocalPlayer* LP = PC->GetLocalPlayer())
 		{
-			UEnhancedInputLocalPlayerSubsystem* InputSystem = LP->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>();
-			if (IsValid(InputAction) && InputSystem)
-			{
-				TArray<UInputModifier*> Modifiers;
-				TArray<UInputTrigger*> Triggers;
-				InputSystem->InjectInputVectorForAction(InputAction, InputValue, Modifiers, Triggers);
-				return ETestNodeActionResult::Success;
-			}
+			InputSystem = LP->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>();
 		}
 	}
-	return ETestNodeActionResult::Failed;
+	if (!IsValid(InputSystem))
+		return ETestNodeActionResult::Failed;
+
+	return ETestNodeActionResult::Ongoing;
+}
+
+ETestNodeActionResult UTestNodeAction_InputAction::OnTestNodeTick(const FVector& Node, TObjectPtr<APawn> Pawn, float DeltaTime)
+{
+	if (CurrentState == EInputActionState::PreAction)
+	{
+		if (CurrentPreActionDuration < PreActionDelay)
+		{
+			CurrentPreActionDuration += DeltaTime;
+			return ETestNodeActionResult::Ongoing;
+		}
+
+		CurrentState = EInputActionState::Press;
+		if (FMath::IsNearlyZero(HoldDuration))
+			CurrentState = EInputActionState::Hold;
+	}
+
+	if (CurrentState == EInputActionState::Press)
+	{
+		InjectInputAction();
+		CurrentState = EInputActionState::PostAction;
+	}
+
+	if (CurrentState == EInputActionState::Hold)
+	{
+		InjectInputAction();
+		if (CurrentHoldDuration < HoldDuration)
+		{
+			CurrentHoldDuration += DeltaTime;
+			return ETestNodeActionResult::Ongoing;
+		}
+		CurrentState = EInputActionState::PostAction;
+	}
+	
+	if (CurrentState == EInputActionState::PostAction)
+	{
+		if (CurrentPostActionDuration < PostActionDelay)
+		{
+			CurrentPostActionDuration += DeltaTime;
+			return ETestNodeActionResult::Ongoing;
+		}
+		return ETestNodeActionResult::Success;
+	}
+
+	return ETestNodeActionResult::Ongoing;
+}
+
+void UTestNodeAction_InputAction::InjectInputAction()
+{
+	TArray<UInputModifier*> Modifiers;
+	TArray<UInputTrigger*> Triggers;
+	InputSystem->InjectInputVectorForAction(InputAction, InputValue, Modifiers, Triggers);
 }
--- Source/PoleStar/Public/TestNodeAction_InputAction.h
+++ Source/PoleStar/Public/TestNodeAction_InputAction.h
@@ -5,6 +5,7 @@
 #include "TestNodeAction_InputAction.generated.h"
 
 class UInputAction;
+class UEnhancedInputLocalPlayerSubsystem;
 
 UCLASS()
 class POLESTAR_API UTestNodeAction_InputAction : public UTestNodeAction
@@ -18,5 +19,36 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Functional Testing")
 	TObjectPtr<const UInputAction> InputAction;	
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Functional Testing", AdvancedDisplay)
+	float HoldDuration = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Functional Testing", AdvancedDisplay)
+	float PreActionDelay = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Functional Testing", AdvancedDisplay)
+	float PostActionDelay = 0.0f;
+
+	virtual ETestNodeActionResult OnTestNodeStart(const FVector& Node, TObjectPtr<APawn> Pawn) override;
 	virtual ETestNodeActionResult OnTestNodeTick(const FVector& Node, TObjectPtr<APawn> Pawn, float DeltaTime) override;
+
+private:
+	enum class EInputActionState
+	{
+		PreAction,
+		Press,
+		Hold,
+		PostAction,
+	};
+
+	EInputActionState CurrentState = EInputActionState::PreAction;
+	float CurrentPreActionDuration = 0.0f;
+	float CurrentHoldDuration = 0.0f;
+	float CurrentPostActionDuration = 0.0f;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UEnhancedInputLocalPlayerSubsystem> InputSystem;
+
+	void InjectInputAction();
+
+
 };
